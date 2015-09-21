28ab7d4434a1f232cc2d506ddcb81deb0a2fc500
Thomas Taylor <thomas.taylor@kineo.com>
2015-09-21 19:29:13 +0100

    Export config to file on change

--- js/config.js
+++ js/config.js
@@ -1,32 +1,44 @@
+var _ = require("underscore");
+var async = require("async");
+var fs = require("fs-extra");
 var path = require("path");
 
 /*
 * Global configuration object, builds on the package.json and _config.json
 */
-var config = module.exports = {};
+var config = module.exports = {
+    _CLI_ROOT: path.dirname(require.main.filename)
+};
 
-(function init(){
-    // CLI JSON
-    addConfigFile("../package.json");
+config.set = module.exports.set = function(toSet) {
+    for(var key in toSet)
+        config[key] = toSet[key];
 
-    config._CLI_ROOT = path.dirname(require.main.filename);
-    config._OUTPUT_DIR = path.join(config._CLI_ROOT, "site");
-    config._SRC_DIR = config.siteSrc || path.join(config._CLI_ROOT, "src");
-    config._PAGES_DIR = path.join(config._SRC_DIR, "_pages");
-    config._POSTS_DIR = path.join(config._SRC_DIR, "_posts");
-    config._DRAFTS_DIR = path.join(config._POSTS_DIR, "drafts");
-    config._TEMPLATES_DIR = path.join(config._SRC_DIR, "_templates");
-    config._POST_ASSETS_DIR = path.join(config._SRC_DIR, "assets");
+    fs.writeFileSync(path.join(config._CLI_ROOT, 'config.json'), JSON.stringify(config, null, "    "));
+};
 
-    // Site JSON
-    addConfigFile(path.join(config._SRC_DIR, "_config.json"));
+(function init() {
+        // CLI config
+        addConfigFile("../package.json");
+
+        // directories
+        config._OUTPUT_DIR = path.join(config._CLI_ROOT, "site");
+        config._SRC_DIR = config.siteSrc || path.join(config._CLI_ROOT, "src");
+        config._PAGES_DIR = path.join(config._SRC_DIR, "_pages");
+        config._POSTS_DIR = path.join(config._SRC_DIR, "_posts");
+        config._JS_DIR = path.join(config._SRC_DIR, "_js");
+        config._LESS_DIR = path.join(config._SRC_DIR, "_less");
+        config._DRAFTS_DIR = path.join(config._POSTS_DIR, "drafts");
+        config._TEMPLATES_DIR = path.join(config._SRC_DIR, "_templates");
+        config._POST_ASSETS_DIR = path.join(config._SRC_DIR, "assets");
+
+        // Site config
+        addConfigFile(path.join(config._SRC_DIR, "_config.json"));
 })();
 
-function addConfigFile(filename) {
+function addConfigFile(filename, cbAdded) {
     try {
         var fileJSON = require(filename);
-        for(var key in fileJSON) {
-            if(!config[key]) config[key] = fileJSON[key];
-        }
+        config.set(fileJSON);
     } catch(e) { console.log("error: " + e); }
 };
