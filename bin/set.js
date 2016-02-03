83a6c8e41bf303cce1c54cb8ddcd992aaff9b8ef
Thomas Taylor <thomas.taylor@kineo.com>
2016-02-03 12:53:44 +0000

    Amend set description

--- bin/set.js
+++ bin/set.js
@@ -3,7 +3,7 @@ var packageJSON = require("../package.json");
 
 /*
 * @name set
-* @description Set config options from the command line
+* @description Sets config options from the command line
 */
 module.exports = function set(args, cbDone) {
     var toSet = args.slice(1);
