802d7e02eb2c4b202f86f5a595a5d3dee52a78fa
Thomas Taylor <thomas.taylor@kineo.com>
2017-01-15 23:57:17 +0000

    Fix whitespace

--- js/helpers.js
+++ js/helpers.js
@@ -7,27 +7,27 @@ var utils = require("./utils");
 */
 
 handlebars.registerHelper("log", function(value) {
-    logger.debug("hbs.log: " + JSON.stringify(value, null, ' '));
+  logger.debug("hbs.log: " + JSON.stringify(value, null, ' '));
 });
 
 handlebars.registerHelper("dateFormat", function(value) {
-    return utils.formatDate(value, "DD/MM/YYYY");
+  return utils.formatDate(value, "DD/MM/YYYY");
 });
 
 handlebars.registerHelper("lowerCase", function(value) {
-    if(value) return value.toLowerCase();
+  if(value) return value.toLowerCase();
 });
 
 handlebars.registerHelper("newerPageLink", function(value) {
-    var no = value-1;
-    return "/blog" + (no == 1 ? "" : "/page" + no);
+  var no = value-1;
+  return "/blog" + (no == 1 ? "" : "/page" + no);
 });
 
 handlebars.registerHelper("olderPageLink", function(value) {
-    return "/blog/page" + (++value);
+  return "/blog/page" + (++value);
 });
 
 handlebars.registerHelper('ifCond', function(v1, v2, options) {
-    if(v1 === v2) return options.fn(this);
-    return options.inverse(this);
+  if(v1 === v2) return options.fn(this);
+  return options.inverse(this);
 });
--- js/utils.js
+++ js/utils.js
@@ -8,23 +8,23 @@ var moment = require("moment");
 var exports = module.exports = {};
 
 exports.formatDate = function(date,format) {
-    var d = new Date(date);
-    if(isNaN(d.getTime())) logger.error("utils.formatDate: Invalid date passed");
-    return moment(d).format(format);
+  var d = new Date(date);
+  if(isNaN(d.getTime())) logger.error("utils.formatDate: Invalid date passed: '" + date + "'");
+  return moment(d).format(format);
 };
 
 exports.fileFilter = function(array, config) {
-    return _.filter(array, function(filename) {
-        var match = true;
+  return _.filter(array, function(filename) {
+      var match = true;
 
-        if(config.type) {
-            filetype = filename.slice(filename.lastIndexOf("."));
-            match = filetype === config.type;
-        }
-        if(config.hidden) {
-            match = filename[0] === ".";
-        }
+      if(config.type) {
+        filetype = filename.slice(filename.lastIndexOf("."));
+        match = filetype === config.type;
+      }
+      if(config.hidden) {
+        match = filename[0] === ".";
+      }
 
-        return match;
-    });
+      return match;
+  });
 };
