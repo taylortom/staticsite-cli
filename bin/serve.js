50c683ef7506ff8c686cdaed7780190ab0321d61
Thomas Taylor <thomas.taylor@kineo.com>
2017-10-21 12:15:48 +0100

    Refactor function for module use

--- bin/serve.js
+++ bin/serve.js
@@ -11,7 +11,7 @@ var logger = require("../js/logger");
 * @name serve
 * @description Runs local server and opens in browser
 */
-module.exports = function serve(args) {
+module.exports = function serve(args, done) {
   var serve = serveStatic(config._OUTPUT_DIR);
   var server = http.createServer(function onRequest(req, res) {
     var done = finalhandler(req, res);
@@ -21,7 +21,13 @@ module.exports = function serve(args) {
   var port = config.testing && config.testing.serverPort || Math.round(Math.random()*1000)+8000;
 
   server.listen(port);
-
   logger.task(`Running localhost at ${logger.var(config._OUTPUT_DIR)}: ${port}`);
-  open("http://localhost:" + port);
+
+  if(typeof done === 'function') {
+    done(null, {
+      url: `http://localhost:${port}`
+    });
+  } else {
+    open("http://localhost:" + port);
+  }
 };
