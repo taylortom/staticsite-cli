609c5ccbd7579b7a04b827713a8285fb69b01055
Thomas Taylor <thomas.taylor@kineo.com>
2017-10-22 12:17:24 +0100

    Amend whitespace

--- bin/less.js
+++ bin/less.js
@@ -12,8 +12,9 @@ var config = require('../js/config');
 */
 module.exports = function less(args, cbCompiled) {
   fs.readFile(path.join(config._LESS_DIR,config.theme.main), "utf-8", function onRead(error, file) {
-    if(error) return cbCompiled(error);
-
+    if(error) {
+      return cbCompiled(error);
+    }
     var options = config.theme.options;
     options.paths = [ config._LESS_DIR ];
 
@@ -22,7 +23,6 @@ module.exports = function less(args, cbCompiled) {
       cleanCSSPlugin = new cleanCSS({ advanced: true });
       options.plugins.push(cleanCSSPlugin);
     }
-
     LESS.render(file, options, function (error, output) {
       if(error) {
         error.message = `Failed to render LESS. ${error.message} (at ${error.filename})`;
