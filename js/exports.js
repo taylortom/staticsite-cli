8b5b656a0d12526d44f8fae2526a99f35f4bffd0
Thomas Taylor <thomas.taylor@kineo.com>
2017-10-24 11:00:16 +0100

    Promisify functions to allow distributed processing in UI

--- js/exports.js
+++ js/exports.js
@@ -4,17 +4,24 @@ var exports = {};
 
 ['build','serve'].forEach(function(name) {
   exports[name] = function(args, cb) {
-    if(typeof args === 'function') {
-      cb = args;
-      args = {};
-    }
-    if(args.dir && fs.existsSync(args.dir)) {
-      process.env.SITE_SRC = args.dir;
-      var config = require('./config');
-      config.initialise(); // needed to make sure we're using the new src dir
-      config._CLI_MODE = false;
-    }
-    require(`../bin/${name}`)(args, cb);
+    return new Promise(function(resolve, reject) {
+      if(typeof args === 'function') {
+        cb = args;
+        args = {};
+      }
+      if(args.dir && fs.existsSync(args.dir)) {
+        process.env.SITE_SRC = args.dir;
+        var config = require('./config');
+        config.initialise(); // needed to make sure we're using the new src dir
+        config._CLI_MODE = false;
+      }
+      require(`../bin/${name}`)(args, function(error, data) {
+        if(error) reject(error);
+        else resolve(data);
+
+        cb(error, data);
+      });
+    });
   }
 });
 
