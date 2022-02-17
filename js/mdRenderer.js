e102306156044d4e6ea18a864e52c154a4d36b78
Tom Taylor <hello@tomtaylor.name>
2022-02-17 12:30:39 +0000

    Rewrite for new Marked API

--- js/mdRenderer.js
+++ js/mdRenderer.js
@@ -4,60 +4,45 @@ var path = require("path");
 
 var config = require("./config");
 
+function imageReplace(s) {
+  return s.replace('file://', `${config.globals.assetsDir}${path.sep}`);
+}
 /*
 * Takes markdown-formatted data, and converts to an HTML string.
 */
-var exports = module.exports = function render(text) {
-  // custom replacements
-  for(var key in replacements)
-    text = text.replace(replacements[key].match, replacements[key].replace);
-
-  return marked(text, { renderer: renderer });
-};
-
-var replacements = {
-  localFile: {
-    match: "file://",
-    replace: config.globals.assetsDir + path.sep
-  }
-};
-
-/**
-* Marked renderer overrides
-*/
-var renderer = new marked.Renderer();
-
-renderer.heading = function(text, level) {
-  var tag = `h${level}`;
-  return `<${tag}>${text}</${tag}>`;
-};
-
-renderer.code = function(value) {
-  return `<div class="source_code" style="white-space: pre-wrap;">${hl(value).value}</div>`;
-};
-
-renderer.codespan = function(value) {
-  return `<span class="source_code inline">${value}</span>`;
-};
-
-renderer.blockquote = function(value) {
-  return `<div class="blockquote">${value}</div>`;
-};
-
-renderer.image = function(href, title, alt) {
-  var src = href.replace(replacements.localFile.match, replacements.localFile.replace);
-  return `<a href="${href}"><img class='media' title="${title}" alt="${alt}" src="${src}" /></a>`;
-};
-
-// TODO this is only block-level
-renderer.html = function(value) {
-  if(!value) return;
-  var name = value.match(/<(.*?)\s/)[1];
-  //TODO allow for ' or "?
-  switch(name) {
-    case "youtube":
-      return `<div class='youtubeWrapper'><div class='inner'><iframe frameborder='0' allowfullscreen='' src='https://www.youtube.com/embed/${value.match(/video-id="(.*)"/)[1]}?rel=0&amp;showinfo=0' allowfullscreen></iframe></div></div>`;
-    default:
-      return value;
-  }
+module.exports = function render(text) {
+  // overrides for the Marked HTML renderer
+  marked.use({ 
+    renderer: {
+      heading(value, level) {
+        var tag = `h${level}`;
+        return `<${tag}>${value}</${tag}>`;
+      },
+      code(value) {
+        return `<div class="source_code" style="white-space: pre-wrap;">${hl(value).value}</div>`;
+      },
+      codespan(value) {
+        return `<span class="source_code inline">${value}</span>`;
+      },
+      blockquote(value) {
+        return `<div class="blockquote">${value}</div>`;
+      },
+      image(href, title, alt) {
+        return `<a href="${href}"><img class='media' title="${title}" alt="${alt}" src="${imageReplace(href)}" /></a>`;
+      },
+      html(value) { // TODO this is only block-level
+        if(!value) return;
+        var match = value.match(/<(.*?)\s/);
+        if(match) {
+          var name = value.match(/<(.*?)\s/)[1];
+          if(name === "youtube") {
+            return `<div class='youtubeWrapper'><div class='inner'><iframe frameborder='0' allowfullscreen='' src='https://www.youtube.com/embed/${value.match(/video-id="(.*)"/)[1]}?rel=0&amp;showinfo=0' allowfullscreen></iframe></div></div>`;
+          }
+        }
+        return value;
+      }
+    }
+  });
+  text = imageReplace(text);
+  return marked.parse(text);
 };
