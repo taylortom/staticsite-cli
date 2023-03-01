f726975b0856870a743b08c286a26f8330df31fd
Thomas Taylor <thomas.taylor@kineo.com>
2023-03-01 23:51:46 +0000

    Refactor serve function

--- bin/serve.js
+++ bin/serve.js
@@ -9,15 +9,8 @@ import serveStatic from 'serve-static';
 * @description Runs local server and opens in browser
 */
 export default function serve(args, done) {
-  var serve = serveStatic(config._OUTPUT_DIR);
-  var server = http.createServer(function onRequest(req, res) {
-    console.log('onRequest:', req.originalUrl);
-    var done = finalhandler(req, res);
-    serve(req, res, done);
-  });
-  // use stored value, or get a value > 8000 && < 9000
-  var port = config.server && config.server.port || config.testing && config.testing.serverPort || Math.round(Math.random()*1000)+8000;
-
-  server.listen(port);
-  logger.task(`Running localhost at ${logger.var(config._OUTPUT_DIR)}: ${port}`);
+  var port = config?.server?.port ?? config?.testing?.serverPort ?? Math.round(Math.random()*1000)+8000;
+  http.createServer((req, res) => {
+    serveStatic(config._OUTPUT_DIR)(req, res, finalhandler(req, res));
+  }).listen(port, () => logger.task(`Running localhost at ${logger.var(config._OUTPUT_DIR)}: ${port}`));
 };
