8ee5fbbf1b7394fa6893a3af1e3e8a9d78166a57
Thomas Taylor <thomas.taylor@kineo.com>
2023-03-01 22:46:59 +0000

    Fix compile script

--- bin/compile.js
+++ bin/compile.js
@@ -16,20 +16,24 @@ export default function compile(args, cbCompiled) {
 
 function compilePages(args, cbCompiled) {
   async.forEachOf(config.pages, function iterator(page, key, cbDoneLoop) {
-    var p = new (getPageModule(key))(key, page, args);
-    async.parallel([
-      _.bind(p.loadTemplates, p),
-      _.bind(p.loadData, p)
-    ], function(error) {
-      if(error) return cbDoneLoop(error);
-      p.write(cbDoneLoop);
-    });
+    getPageModule(key, (e, Page) => {
+      console.log(Page);
+      var p = new Page(key, page, args);
+      async.parallel([
+        _.bind(p.loadTemplates, p),
+        _.bind(p.loadData, p)
+      ], function(error) {
+        if(error) return cbDoneLoop(error);
+        p.write(cbDoneLoop);
+      });
+    })
   }, cbCompiled);
 }
 
-function getPageModule(key) {
+async function getPageModule(key, cbLoaded) {
   // look for custom page first, then fall back to default
-  try { var Page = require("../js/compile-" + key); }
-  catch(e) { var Page = require("../js/compile-page"); }
-  return Page;
+  let Page;
+  try { Page = await import(`../js/compile-${key}.js`); }
+  catch(e) { Page = await import('../js/compile-page.js'); }
+  cbLoaded(null, Page.default);
 }
