9d95689e3642dfd5025ea30f191e214933ee0ac1
Thomas Taylor <thomas.taylor@kineo.com>
2023-03-01 22:52:04 +0000

    Refactor utils

--- js/utils.js
+++ js/utils.js
@@ -1,29 +1,28 @@
-import _ from 'underscore';
 import logger from './logger.js';
 import moment from 'moment';
 
 /*
 * various helper functions
 */
-var exports = {};
 
-exports.formatDate = function(date,format) {
-  var d = new Date(date);
-  if(isNaN(d.getTime())) logger.error("utils.formatDate: Invalid date passed: '" + date + "'");
-  return moment(d).format(format);
+const Utils = {
+  formatDate: (date,format) => {
+    var d = new Date(date);
+    if(isNaN(d.getTime())) logger.error(`utils.formatDate: Invalid date passed: '${date}'`);
+    return moment(d).format(format);
+  },
+  fileFilter: (array, config) => {
+    array.filter(f => {
+      var match = true;
+      if(config.type) {
+        match = filename.slice(filename.lastIndexOf(".")) === config.type;
+      }
+      if(config.hidden) {
+        match = filename[0] === ".";
+      }
+      return match;
+    });
+  }
 };
 
-exports.fileFilter = function(array, config) {
-  return _.filter(array, function(filename) {
-    var match = true;
-    if(config.type) {
-      match = filename.slice(filename.lastIndexOf(".")) === config.type;
-    }
-    if(config.hidden) {
-      match = filename[0] === ".";
-    }
-    return match;
-  });
-};
-
-export default exports;
+export default Utils;
