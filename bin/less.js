26f574a515eef80fc0e925065548f26cfa2fd577
Thomas Taylor <thomas.taylor@kineo.com>
2023-03-01 22:51:57 +0000

    Fix less task

--- bin/less.js
+++ bin/less.js
@@ -2,13 +2,13 @@ import fs from 'fs-extra';
 import cleanCss from 'less-plugin-clean-css';
 import config from '../js/config.js';
 import path from 'path';
-import { render as renderLess } from 'less';
+import less from 'less';
 
 /*
 * @name less
 * @description Compiles the less to the output folder
 */
-export default function less(args, cbCompiled) {
+export default function(args, cbCompiled) {
   fs.readFile(path.join(config._LESS_DIR,config.theme.main), "utf-8", function onRead(error, file) {
     if(error) {
       return cbCompiled(error);
@@ -18,10 +18,9 @@ export default function less(args, cbCompiled) {
 
     if(options.compress === true) {
       if(!options.plugins) options.plugins = [];
-      cleanCSSPlugin = new cleanCss({ advanced: true });
-      options.plugins.push(cleanCSSPlugin);
+      options.plugins.push(new cleanCss({ advanced: true }));
     }
-    renderLess(file, options, function (error, output) {
+    less.render(file, options, function (error, output) {
       if(error) {
         error.message = `Failed to render LESS. ${error.message} (at ${error.filename})`;
         return cbCompiled(error);
