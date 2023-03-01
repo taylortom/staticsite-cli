18c0a8fa0f12634f5378daa63dc0d2119eba0bb8
Thomas Taylor <thomas.taylor@kineo.com>
2023-03-01 22:35:55 +0000

    Fix copy task

--- bin/copy.js
+++ bin/copy.js
@@ -9,16 +9,13 @@ import path from 'path';
 * @description Copies files/folders in _SRC_DIR not prefixed with _ or . or blacklisted
 */
 export default function copy(args, cbCopied) {
-  var { blacklist } = require('../package.json');
-
   fs.readdir(config._SRC_DIR, function read(error, files) {
     if(error) return cbCopied(error);
 
     async.each(files, function iterator(file, cbDoneLoop) {
       var options = { clobber: true, preserveTimestamps: true };
-      var blacklisted = blacklist.indexOf(file) !== -1;
-      // skip files beginning with . or _
-      if(blacklisted || file.match(/^\.|^_/)) {
+      // skip blacklisted files/files beginning with . or _
+      if(config.blacklist.includes(file) || file.match(/^\.|^_/)) {
         return cbDoneLoop();
       }
       fs.copy(path.join(config._SRC_DIR, file), path.join(config._OUTPUT_DIR, file), options, function(error) {
